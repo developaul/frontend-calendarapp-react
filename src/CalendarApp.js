8e91f3740a060a1a483d44dfcb263197697d692e
Paul Chávez <paulchavezromero20@gmail.com>
2021-02-22 13:15:16 -0500

    LoginScreen added

---
+++ src/CalendarApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppRouter from './routers/AppRouter';
+
+const CalendarApp = () => {
+
+    return (
+        <>
+            <AppRouter />
+        </>
+    )
+
+}
+
+export default CalendarApp;
